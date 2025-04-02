8217d62493517c2fb478db04b853136d8e039f85
Frame-Code <moraisurcan@ghotmail.com>
2025-04-01 22:07:31 -0500

    Singleton pattern completed

--- Singleton/src/DatabaseConnector.java
+++ Singleton/src/DatabaseConnector.java
@@ -1,4 +1,9 @@
-/** El objeto se crea dentro de la misma clase
+/** Defining a Singleton class
+ * Is necessary to define a static attribute type this class (This attribute will be the Instance of this class).
+ * The constructor of this class will be private to negate the new objects creation of this class.
+ * Finally, to define the method getInstance() to return the unique instance of this class, if the variable previously mentioned
+ * isn't defined, the method define it, create it and return the object necessary.
+ *
  * @author Daniel Mora Cantillo
  */
 public class DatabaseConnector {
@@ -14,5 +19,10 @@ public class DatabaseConnector {
         }
 
         return new DatabaseConnector();
+    }
+
+    @Override
+    public String toString() {
+        return "DatabaseConnector{}";
     }
 }
--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -1,9 +1,16 @@
 /**
+ * The singleton pattern allows to create a unique object of any class in all the program,
+ * to access it (the unique instance) in any part of the program
+ *
  * @author Daniel Mora Cantillo
  */
 public class Main {
     public static void main(String[] args) {
+        //The variables are different but the object DatabaseConnector is the same
         DatabaseConnector connector = DatabaseConnector.getInstance();
-        System.out.println("Hello, World!");
+        DatabaseConnector connector1 = DatabaseConnector.getInstance();
+
+        System.out.println(connector);
+        System.out.println(connector1);
     }
 }
